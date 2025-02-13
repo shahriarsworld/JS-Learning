30a6fc07c5eea81daae0cf352d1122887f3f39cb
Md Shahriar Hossain <113468427+shahriarsworld@users.noreply.github.com>
2025-02-13 23:05:06 +0600

    Add files via upload

---
+++ mapFilterReduce.js
@@ -0,0 +1,12 @@
+//using map
+
+//doubling numbers
+const numbers = [2,5,6,8,9,10];
+const doubled = numbers.map(num => num*3);
+console.log(doubled);
+
+//Uppercasing names
+const names = ["asif", "emon", "zeux", "necromancer"];
+const upperC = names.map(name => name.toUpperCase());
+console.log(upperC);
+
